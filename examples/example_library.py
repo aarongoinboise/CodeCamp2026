238876f843585bd8defd06e1080ae8c10c9378ea
aarongoinboise <aarongoin@u.boisestate.edu>
2026-04-18 13:54:02 -0600

    change library example

--- examples/example_library.py
+++
@@ -1,4 +0,0 @@
-import cbbpy.mens_scraper as mens
-
-game_info = mens.get_game_info('401856600')
-print(game_info)
---
+++ examples/example_python_library.py
@@ -0,0 +1,5 @@
+import cbbpy.mens_scraper as s
+
+print(s.get_team_schedule('michigan', 2026))
+# print(s.get_team_schedule('michigan', 2025))
+# print(s.get_game_info('401746069'))
