bd022a71f409e582067c6ed26dfe6731e740cff8
aarongoin <aarongoin@u.boisestate.edu>
2026-04-26 14:05:24 -0600

    more general manual scrape

--- examples/example_scrape_manual.py
+++ examples/example_scrape_manual.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 from pprint import pprint
 
 url = "https://www.sports-reference.com/cbb/boxscores/2026-04-06-20-michigan.html"
-
 res = requests.get(url)
 soup = BeautifulSoup(res.text, "html.parser")
 
@@ -12,19 +11,20 @@ for table in soup.find_all("table"):
     if not tbody:
         continue
 
+    # grab headers from thead
+    headers = []
+    thead = table.find("thead")
+    if thead:
+        for th in thead.find_all(["th", "td"]):
+            headers.append(th.get_text(strip=True))
+
     for row in tbody.find_all("tr"):
         cells = row.find_all(["th", "td"])
         if not cells:
             continue
 
-        data = {}
-
-        for cell in cells:
-            key = cell.get("data-stat")
-            if not key:
-                continue
-
-            data[key] = cell.get_text(strip=True)
+        values = [cell.get_text(strip=True) for cell in cells]
+        data = dict(zip(headers, values))
 
         if data:
             pprint(data)
